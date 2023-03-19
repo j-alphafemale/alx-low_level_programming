b8506cacc4c2de80049bb90e66abdfbd14c87ce3
j-alphafemale <mrefujoanna@gmail.com>
2023-03-19 11:50:14 -0700

    correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
-* main - prints the alphabet in lowercase,
-* followed by a new line
-* Return: Always 0 (Success)
-*/
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
+int n = 97;
+
+while (n <= 122)
 {
-putchar(ch)
+putchar(n);
+n++;
 }
-putchar('\n');
 return (0);
 }
